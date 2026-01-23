23f9c67c60ed8a087f98df578a575c35c0472ec4
inTR4NCE <tanishqsethi760@gmail.com>
2026-01-23 13:23:58 +0530

    feat:Added TDZ code

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-const container = document.querySelector('.container')
-const outer = document.querySelector('.outer')
-const button = document.querySelector('.submit')
+// // // const container = document.querySelector('.container')
+// // // const outer = document.querySelector('.outer')
+// // // const button = document.querySelector('.submit')
 
-container.addEventListener('click', () => {console.log('container clicked')})
-outer.addEventListener('click', () => {console.log('outer clicked')})
-button.addEventListener('click', () => {console.log('button clicked')})
+// // // container.addEventListener('click', () => {console.log('container clicked')})
+// // // outer.addEventListener('click', () => {console.log('outer clicked')})
+// // // button.addEventListener('click', () => {console.log('button clicked')})
+
+// // console.log(b)
+
+// // let a = 3554543534
+// // var b = 454535352556
+
+// // console.log(b)
+// // console.log(a)
+
+// // function print(){
+// //     console.log("hello world")  
+// // }
+
+// // print() 
+
+
+
+// function first(){
+//     second()
+// }
+// function second(){
+//     third()
+// }
+
+// first()
+
+
+function infinite(){
+    infinite()
+}
+infinite()
