654427b8ab64d921c7f9170a532201b646eb9aa1
inTR4NCE <tanishqsethi760@gmail.com>
2026-01-29 11:27:19 +0530

    New js

--- script.js
+++ script.js
@@ -1,158 +1,22 @@
-// let obj={
-//     name:"Devendra",
-//     address:"Gurgaon",
-//     age:23
-// }
-// let obj2={
-//     name:"Devendra",
-//     address:"Gurgaon",
-//     age:23
-// }
-// obj.age=50
-// console.log(obj)
+const nameInput = document.querySelector("#name");
+const queryBtn = document.querySelector(".btn");
+const list = document.querySelector(".list");
 
-// const para=document.querySelector(".para")
-// para.innerText="this is updated one"
-// console.info(document)
+queryBtn.addEventListener("click", function () {
+    if (nameInput.value === "") return; 
 
-// console.log("Helllo Studentss!")
+    const li = document.createElement("li");
+    const dlt = document.createElement("button");
 
-// let obj={
-//     type:"Wooden",
-//     color:"Brown",
-//     length:"7ft"
-// }
+    li.innerText = nameInput.value;
+    dlt.innerText = "Delete";
 
-// let obj2={
-//     type:"iron",
-//     color:"Brown",
-//     length:"7ft"
-// }
+    dlt.addEventListener("click", function () {
+        list.removeChild(li);
+    });
 
-// obj.type="Plastic"
-// console.log(obj)
+    li.appendChild(dlt);
+    list.appendChild(li);
 
-// console.log(document)
-
-// const para=document.querySelector(".para")
-// para.innerText="This is a new paragraph"
-// para.style.color="green"
-// console.log(para)
-
-// console.log(document)
-
-
-// const para=document.getElementById("para")
-// para.innerText="This is a new paragraph"
-// para.style.color="green"
-// console.log(para)
-
-// const para2=document.getElementsByClassName("para")  //return HTMLCollection
-// para2[0].innerText="This is a new paragraph"
-// para2[1].style.color="green"
-// console.log(para2)
-
-// const para=document.querySelectorAll(".para")  //return NodeList
-// para[0].textContent="This is a new paragraph"
-// para[1].style.color="green"
-// console.log(para)
-
-// let arr=[23,54,46,232,564,23]
-
-// // const newArr=arr.map((ele)=>ele)  //it returns a new array
-// arr.forEach((ele)=>console.log(ele))  //it does not returns a new array
-
-
-// const container=document.querySelector(".container")
-// container.innerHTML="<h1>This is a Heading</h1>"
-// console.log(container)
-
-// function message(event)
-// {
-//     console.log(event.key)
-//     // alert("You have clicked the button")
-// }
-
-// const btn=document.querySelector("button")
-// btn.classList.add("btn")
-// btn.classList.remove("btn")
-// btn.addEventListener('click',message)
-// btn.addEventListener('keydown',message)
-// btn.addEventListener('keyup',message)
-// btn.removeEventListener('click',message)
-
-// const form=document.querySelector("form")
-// form.addEventListener('submit',function(event){
-//     event.preventDefault()
-//     console.log("Form Submitted")
-// })
-
-// const container=document.querySelector('.container')
-// const outer=document.querySelector('.outer')
-// const button=document.querySelector('button')
-
-
-// container.addEventListener('click',()=>{console.log("Div")},true)
-// outer.addEventListener('click',()=>{console.log("Outer Div")},true)
-// button.addEventListener('click',()=>{console.log("Button")},true)
-
-
-// console.log("New Code")
-// debugger
-// console.log(b)
-// // console.log(a)
-
-// let a=23534
-// var b=5674
-
-// console.log(a)
-// console.log(b)
-// function print(){
-//     var c=345
-//     console.log(c)
-//     console.log("Inside fn")
-// }
-
-// print()
-
-
-// function first(){
-//     second()
-// }
-// function second(){
-//     third()
-// }
-// function third(){
-//     console.trace()
-// }
-// first()
-
-// function infinite(){
-//     infinite()
-// }
-// infinite()
-
-// console.log(a)
-// console.log(b)
-// // console.log(c)
-
-// var a=345
-// let b=5674
-
-// console.log(a)
-// console.log(b)
-
-// let total=40
-
-// function calculate(){
-//     console.log(total)
-// //     let total=100
-// // }
-// // calculate()
-
-
-
-// console.log("First Line")
-// setTimeout(()=>{console.log("after 2 sec")},2000)
-// console.log("Second Line")
-
+    nameInput.value = "";
+});
